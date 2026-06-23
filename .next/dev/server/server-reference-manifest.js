7fd44f192311fbf839355c6ac8770d631c0deb98
manofukui <manofukui.2021@gmail.com>
2026-06-23 16:21:46 +0900

    fix: .nextフォルダをGit管理から除外し.gitignoreを修正

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"S5HffwrZMA+ejGJoN1eCNSu1a+akLU5nqBQptSOKDVw=\"\n}"
--- .next/dev/static/chunks/src_app_login_page_tsx_004tgyl._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_1k03df3._.js","static/chunks/node_modules_react-hook-form_dist_index_esm_mjs_0b0kr13._.js","static/chunks/node_modules_zod_v4_1ujpcun._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_@supabase_postgrest-js_dist_index_mjs_0pzlnq8._.js","static/chunks/node_modules_@supabase_auth-js_dist_module_1vqrmkg._.js","static/chunks/node_modules_1qkkjqm._.js","static/chunks/src_15nml1p._.js"],
-    source: "dynamic"
-});
